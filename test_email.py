c62351af58668aba4463236f973c0e06266bd8ca
Cavin Dsouza <cavin46016@gmail.com>
2018-02-18 04:46:03 -0500

    Update test_email.py

--- test_email.py
+++ test_email.py
@@ -1,7 +1,7 @@
 from send_email import SendEmail
 
 def main():       
-    """Connect to smtp.gmail.com on port 587 since we are using TLS.)"""     
+    """Connect to smtp.gmail.com on port 587 for TLS; 465 for SSL)"""     
     
     s = SendEmail(['smtp.gmail.com', 587, 'abc@gmail.com', 'abc123']);
     s.send(to = ['xyz@gmail.com'])
