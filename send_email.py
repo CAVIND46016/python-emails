6772df1685579f891d92d87fd8dbff13b8410481
Cavin Dsouza <cavin46016@gmail.com>
2018-03-18 22:09:56 -0400

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,48 +1,57 @@
+"""
+Creates a SMTP connection and sends an email.
+"""
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
-from validate_email import validate_email
 from email import encoders
 import os.path as op
-import smtplib
 import socket
 
+import smtplib
+from validate_email import validate_email
 
 class SendEmail:
-    def __init__(self, conn_param, use_TLS = True):
+    """
+    Contains the logic for sending an email.
+    """
+    def __init__(self, conn_param, use_TLS=True):
         """
             conn_param = [host, port, user, password] in that order. <list>
             use_TLS = Boolean value indicating usage of TLS. <boolean>
         """
-        if(not isinstance(conn_param, list)):
+        if not isinstance(conn_param, list):
             return "Connection parameters are in invalid format."
         
-        self.is_OK = True;
+        self.is_ok = True
         self.default_from_name = conn_param[2]
         
         try:
-            if(use_TLS):
-                self.s = smtplib.SMTP(conn_param[0], conn_param[1])
-                self.s.starttls()
+            if use_TLS:
+                self.smtp_conn = smtplib.SMTP(conn_param[0], conn_param[1])
+                self.smtp_conn.starttls()
             else:
-                self.s = smtplib.SMTP_SSL(conn_param[0], conn_param[1])
+                self.smtp_conn = smtplib.SMTP_SSL(conn_param[0], conn_param[1])
                 
-            self.s.login(conn_param[2], conn_param[3])
-            """This exception is raised for address-related errors, for getaddrinfo() and getnameinfo()."""
+            self.smtp_conn.login(conn_param[2], conn_param[3])
         except socket.gaierror as err: 
+            # This exception is raised for address-related errors, 
+            # for getaddrinfo() and getnameinfo().
             print("Error: {}".format(err))
-            self.is_OK = False;
+            self.is_ok = False
         except smtplib.SMTPNotSupportedError:
             print("SMTP AUTH extension not supported by server.")
-            self.is_OK = False;
+            self.is_ok = False
         except smtplib.SMTPAuthenticationError:
-            print("The server didn't accept the username/password combination provided OR\naccount does not provide access to less secure apps.");
-            self.is_OK = False;
+            print("The server didn't accept the username/password combination provided \
+                    OR\naccount does not provide access to less secure apps.")
+            self.is_ok = False
         except smtplib.SMTPServerDisconnected: 
             print("Connection closed unexpectedly.")
-            self.is_OK = False;
-            
-    def parse_attachments(self, msg, attachments):
+            self.is_ok = False
+    
+    @staticmethod       
+    def parse_attachments(msg, attachments):
         """
             msg: <class 'email.mime.multipart.MIMEMultipart'>
             attachments: a list of full file paths of respective attachments. <list>
@@ -57,21 +66,22 @@ class SendEmail:
             
             msg.attach(part)
     
-    def check_mail_address(self, mail):
+    @staticmethod 
+    def check_mail_address(emails):
         """
             mail: list of email addresses. <list>
         """
-        if(not isinstance(mail, list)):
-            return False;
+        if not isinstance(emails, list):
+            return False
         
-        """Validate each email in the list"""
-        for i in mail:
-            if(not validate_email(i)):
-                return False;
+        for mail in emails:
+            if not validate_email(mail):
+                return False
             
         return True
             
-    def send(self, to, from_name = None, cc = None, bcc = None, subject = '', body = '', attachments = None):
+    def send(self, to_address, from_name=None, cc_address=None, \
+             bcc_address=None, subject='', body='', attachments=None):
         """
             to:            'To' email address <list> ==> mandatory parameter
             ********  Rest are optional parameters  **********
@@ -83,52 +93,51 @@ class SendEmail:
             attachments:   a list of full file paths of respective attachments. <list>
         """
         
-        if(not self.is_OK):
+        if not self.is_ok:
             print("Message sending failed. Connection not found.")
         else: 
             # setup the parameters of the message
             msg = MIMEMultipart()
             
-            if(not from_name):
+            if not from_name:
                 from_name = self.default_from_name
             
-            msg['From'] = from_name; 
+            msg['From'] = from_name 
             
-            if(not to):
+            if not to_address:
                 print("'to' address not specified.")
                 return
             
-            if(not self.check_mail_address(to) or \
-                (cc and not self.check_mail_address(cc)) or \
-                 (bcc and not self.check_mail_address(bcc))):
+            if not self.check_mail_address(to_address) or \
+                (cc_address and not self.check_mail_address(cc_address)) or \
+                 (bcc_address and not self.check_mail_address(bcc_address)):
                 print("Email address is not in required format and/or is invalid.")
                 return
             
-            msg['To']= ', '.join(to);
+            msg['To'] = ', '.join(to_address)
             
-            if(cc):
-                msg['Cc'] = ', '.join(cc);
+            if cc_address:
+                msg['Cc'] = ', '.join(cc_address)
                 
-            if(bcc):
-                msg['Bcc'] = ', '.join(bcc);
+            if bcc_address:
+                msg['Bcc'] = ', '.join(bcc_address)
             
             msg['Subject'] = subject
-
             msg.attach(MIMEText(body, 'plain'))
            
-            if(attachments): 
-                if(isinstance(attachments, list)):
+            if attachments: 
+                if isinstance(attachments, list):
                     self.parse_attachments(msg, attachments)
                 else:
-                    print("Attachments are provided in invalid format.")
+                    print("Attachments are provided in an invalid format.")
             
             # send the message via the server set up earlier.
             try:
-                self.s.send_message(msg)
+                self.smtp_conn.send_message(msg)
             except smtplib.SMTPSenderRefused:
-                print("Your message exceeded allowed message size limits.\nMessage sending failed. Connection not found.");
+                print("Your message exceeded allowed message size limits.\
+                        \nMessage sending failed. Connection not found.")
                 return
             
-            self.s.quit()
-            
+            self.smtp_conn.quit()
             print("Your message has been sent successfully.")
