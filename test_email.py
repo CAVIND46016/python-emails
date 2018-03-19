3f0bb293b36d40e60b2ff0ec51cc9c3bd9661caf
Cavin Dsouza <cavin46016@gmail.com>
2018-03-18 22:22:38 -0400

    Update test_email.py

--- test_email.py
+++ test_email.py
@@ -1,10 +1,15 @@
+"""Connect to smtp.gmail.com on port 587 for TLS, 
+   port 465 for SSL)
+""" 
 from send_email import SendEmail
 
-def main():       
-    """Connect to smtp.gmail.com on port 587 for TLS; 465 for SSL)"""     
-    
-    s = SendEmail(['smtp.gmail.com', 587, 'abc@gmail.com', 'abc123']);
-    s.send(to = ['xyz@gmail.com'])
+def main():           
+    """
+    Entry-point for the function.
+    """
+    send_email = SendEmail(['smtp.gmail.com', 587, 'abc@gmail.com', 'abc123'])
+    send_email.send(to_address=['xyz@gmail.com'])
 
-if(__name__ == "__main__"):
+if __name__ == "__main__":
     main()
+    
