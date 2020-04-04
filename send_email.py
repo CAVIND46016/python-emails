720fc02568c948aee4f0745099b258b7ce6410a1
Cavin Dsouza <21344198+CAVIND46016@users.noreply.github.com>
2020-04-03 21:32:21 -0400

    Add files via upload

--- send_email.py
+++ send_email.py
@@ -10,6 +10,7 @@ import os
 import smtplib
 from validate_email import validate_email
 
+
 class Email:
     """
     Contains the logic for sending an email.
@@ -17,8 +18,8 @@ class Email:
     def __init__(self, **kwargs):
         """
         Args:
-            **kwargs: Keyword arguments with host, port,
-            user, password and use_tls as keys.
+        **kwargs: Keyword arguments with host, port,
+        user, password and use_tls as keys.
         """
         self.__dict__.update(kwargs)
 
@@ -34,14 +35,14 @@ class Email:
                 smtp_conn = smtplib.SMTP_SSL(self.host, self.port)
 
             smtp_conn.login(self.user, self.password)
-        except smtplib.SMTPNotSupportedError as err:
-            raise Exception(f"SMTP AUTH extension not supported by server.\n{err}")
-        except smtplib.SMTPAuthenticationError as err:
-            raise Exception(f"The server didn't accept the username/password " 
+        except smtplib.SMTPNotSupportedError:
+            raise Exception(f"SMTP AUTH extension not supported by server.")
+        except smtplib.SMTPAuthenticationError:
+            raise Exception(f"The server didn't accept the username/password "
                             f"combination provided OR account does not provide "
-                            f"access to less secure apps.\n{err}")
-        except smtplib.SMTPServerDisconnected as err:
-            raise Exception(f"Connection closed unexpectedly.\n{err}")
+                            f"access to less secure apps.")
+        except smtplib.SMTPServerDisconnected:
+            raise Exception(f"Connection closed unexpectedly.")
         except:
             raise Exception("Something went wrong.")
 
@@ -51,8 +52,11 @@ class Email:
     def _parse_attachments(attachments):
         """
         Args:
-            attachments<list>: a list of full file paths of respective attachments.
+        attachments <list/tuple>: a list of full file paths of respective attachments.
         """
+        if attachments and not isinstance(attachments, (list, tuple)):
+            raise ValueError("Attachments are provided in an invalid format.")
+
         parts = []
         for path in attachments:
             part = MIMEBase('application', "octet-stream")
@@ -60,7 +64,7 @@ class Email:
                 part.set_payload(file.read())
             encoders.encode_base64(part)
             part.add_header('Content-Disposition',
-                            'attachment; filename="{}"'.format(os.path.basename(path)))
+                            f'attachment; filename="{os.path.basename(path)}"')
             parts.append(part)
 
         return parts
@@ -68,30 +72,25 @@ class Email:
     @staticmethod
     def _check_mail_address(emails):
         """
-            mail: list of email addresses. <list>
+        mail: list of email addresses. <list>
         """
-        if not isinstance(emails, list):
+        if not isinstance(emails, (list, tuple)):
             return False
 
-        for mail in emails:
-            if not validate_email(mail):
-                return False
-
-        return True
+        return all([validate_email(email) for email in emails])
 
     def send(self, to_address, from_name=None, cc_address=None,
              bcc_address=None, subject='', body='', attachments=None):
         """
         Args:
-            to_address <list>:   'To' email address (mandatory parameter)
-
-            (optional parameters)
-            from_name <str>:     From name to be displayed. Default value is user email address
-            cc_address <list>:   'cc' email address
-            bcc_address <list>:  'bcc' email address
-            subject <str>:       Email subject
-            body <str>:          Email body
-            attachments <list>:  A list of full file paths of respective attachments
+        to_address <list/tuple>:   'To' email address (mandatory parameter)
+        (optional parameters)
+        from_name <str>:     From name to be displayed. Default value is user email address
+        cc_address <list/tuple>:   'cc' email address
+        bcc_address <list/tuple>:  'bcc' email address
+        subject <str>:       Email subject
+        body <str>:          Email body
+        attachments <list/tuple>:  A list of full file paths of respective attachments
         """
         msg = MIMEMultipart()
         msg['From'] = from_name if from_name else self.user
@@ -110,9 +109,6 @@ class Email:
 
         msg['Subject'] = subject
         msg.attach(MIMEText(body, 'plain'))
-
-        if attachments and not isinstance(attachments, list):
-            raise ValueError("Attachments are provided in an invalid format.")
 
         attachment_parts = self._parse_attachments(attachments) if attachments else []
         for part in attachment_parts:
