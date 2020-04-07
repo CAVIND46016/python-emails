5f3da52f715f8c6090ae5b198347da239c9b2b0c
Cavin Dsouza <21344198+CAVIND46016@users.noreply.github.com>
2020-04-07 03:39:59 -0400

    Add files via upload

--- send_email.py
+++ send_email.py
@@ -1,6 +1,8 @@
 """
 Creates a SMTP connection and sends an email.
 """
+from email.header import Header
+from email.utils import formataddr
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
@@ -19,7 +21,7 @@ class Email:
         """
         Args:
         **kwargs: Keyword arguments with host, port,
-        user, password and use_tls as keys.
+                  user, password and use_tls as keys.
         """
         self.__dict__.update(kwargs)
 
@@ -54,7 +56,7 @@ class Email:
         Args:
         attachments <list/tuple>: a list of full file paths of respective attachments.
         """
-        if isinstance(attachments, (list, tuple)):
+        if not isinstance(attachments, (list, tuple)):
             raise ValueError("Attachments are provided in an invalid format.")
 
         parts = []
@@ -63,8 +65,7 @@ class Email:
             with open(path, 'rb') as file:
                 part.set_payload(file.read())
             encoders.encode_base64(part)
-            part.add_header('Content-Disposition',
-                            f'attachment; filename="{os.path.basename(path)}"')
+            part.add_header('Content-Disposition', f'attachment; filename="{os.path.basename(path)}"')
             parts.append(part)
 
         return parts
@@ -79,12 +80,11 @@ class Email:
 
         return all([validate_email(email) for email in emails])
 
-    def send(self, to_address, from_name=None, cc_address=None,
+    def send(self, to_address=None, from_name=None, cc_address=None,
              bcc_address=None, subject='', body='', attachments=None):
         """
         Args:
-        to_address <list/tuple>:   'To' email address (mandatory parameter)
-        (optional parameters)
+        to_address <list/tuple>:   'To' email address
         from_name <str>:     From name to be displayed. Default value is user email address
         cc_address <list/tuple>:   'cc' email address
         bcc_address <list/tuple>:  'bcc' email address
@@ -92,18 +92,18 @@ class Email:
         body <str>:          Email body
         attachments <list/tuple>:  A list of full file paths of respective attachments
         """
-        msg = MIMEMultipart()
-        msg['From'] = from_name if from_name else self.user
+        if not to_address and not cc_address and not bcc_address:
+            raise ValueError("No recipients provided.")
 
-        if not to_address:
-            raise ValueError("'to' address not specified.")
-
-        if not self._check_mail_address(to_address) or \
+        if (to_address and not self._check_mail_address(to_address)) or \
             (cc_address and not self._check_mail_address(cc_address)) or \
              (bcc_address and not self._check_mail_address(bcc_address)):
             raise ValueError("Email address is not in required format and/or is invalid.")
 
-        msg['To'] = ', '.join(to_address)
+        msg = MIMEMultipart()
+        msg['From'] = formataddr((str(Header(from_name, 'utf-8')), self.user)) if from_name else self.user
+
+        msg['To'] = ', '.join(to_address) if to_address else None
         msg['Cc'] = ', '.join(cc_address) if cc_address else None
         msg['Bcc'] = ', '.join(bcc_address) if bcc_address else None
 
@@ -117,7 +117,6 @@ class Email:
         smtp_conn = self._login()
         try:
             smtp_conn.send_message(msg)
-            print("Message sent.")
         except smtplib.SMTPSenderRefused:
             raise Exception("Your message exceeded allowed message size limits.\n"
                             "Message sending failed. Connection not found.")
